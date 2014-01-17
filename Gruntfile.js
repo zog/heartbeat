859fed4df4e8cfc40c7998eaac20fb7d5f791da8
Zog <johan@about-blank.fr>
2014-01-17 18:44:22 +0100

    Never gonna give you up

--- Gruntfile.js
+++ Gruntfile.js
@@ -464,5 +464,5 @@ module.exports = function (grunt) {
     'build'
   ]);
 
-  grunt.registerTask('heroku:production', 'clean less mincss uglify');
+  grunt.registerTask('heroku', 'clean less mincss uglify');
 };
